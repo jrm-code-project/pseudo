01c661753bb61838a78b0eea659f817815429997
Joseph Marshall <joseph.marshall@nutanix.com>
2025-08-10 08:57:14 -0700

    Snapshot

--- tests.lisp
+++ tests.lisp
@@ -7,8 +7,8 @@
 
 (pseudefun my-subtract (left right) "Subtract right from left.")
 
-(pseudefun linear-regression (points)
-   "Performs a linear regression on points.  Points are pairs of (x . y).  Return slope, intercept, and r-squared.")
+;;(pseudefun linear-regression (points)
+;;   "Performs a linear regression on points.  Points are pairs of (x . y).  Return slope, intercept, and r-squared.")
 
 (defun greet (person)
   (format t "~&Hello, ~a, how goes it?~%" person))
